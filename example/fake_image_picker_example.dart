8ba5a7e4e943741dcd0e6f44eb162d9f7af5f578
makjac <maks.jackowski@proton.me>
2026-06-16 17:17:46 +0200

    imital commit

---
+++ example/fake_image_picker_example.dart
@@ -0,0 +1,6 @@
+import 'package:fake_image_picker/fake_image_picker.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/fake_image_picker_test.dart
@@ -0,0 +1,16 @@
+import 'package:fake_image_picker/fake_image_picker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
